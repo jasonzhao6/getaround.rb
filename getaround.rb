e9b93364d936a4583edd895ae4d999ef9e4e07a1
Jason Zhao <jasonzhao6@gmail.com>
2015-08-01 15:32:41 -0700

    Update car list

--- getaround.rb
+++ getaround.rb
@@ -1,11 +1,10 @@
 # DEBUG = true
-# INSTANT_ONLY = true
 # MANUALS_ONLY = true
 # CONVERTIBLES_ONLY = true
 FOR_JASON_ONLY = true
-START_TIME = Time.local(2014,12,12,18,00)
-END_TIME = Time.local(2014,12,13,18,00)
-MAX_AGE = 6
+# START_TIME = Time.local(2015,6,19,14,00)
+# END_TIME = Time.local(2015,6,21,18,30)
+MAX_AGE = 15
 MIN_PRICE = 70
 blacklisted_ids = []
 blacklisted_ids += [
@@ -21,10 +20,17 @@ blacklisted_ids += [
   '100004298771185', # '10 R8 $781
   '100004317911185', # '11 M3 $160
   '100004392791188', # '09 Cayman $136
+  '100004316571185', # '12 SLK 350 $156
+  '100004449271186', # '15 FRS RS 1.0 $66
+  '100004608641187', # '15 GTI $97
+  '100004492541185', # '15 M4 %251
 ] if defined?(FOR_JASON_ONLY) && FOR_JASON_ONLY
 blacklisted_ids += [
   # Slow cars
-  '100004411161186', '100004031701189', '100004276821186', '100004427591185', '100004108281186', '100004429301185', '100004374351186', '100004326751185', '100004150081185', '100004134231186', '100004146891185', '100004132181185', '100004132611185', '4103', '100004127791189', '100004112391187', '100003941273687', '100004091281185', '100003945196933', '100004059181185', '100003941496344', '100004084441185', '100004094671187', '2901', '100004094511189', '100003962361285', '100004021201185', '100004042451193', '100004045911191'
+  '100004454341186', '100004411161186', '100004031701189', '100004276821186', '100004427591185', '100004108281186', '100004429301185', '100004374351186', '100004326751185', '100004150081185', '100004134231186', '100004146891185', '100004132181185', '100004132611185', '4103', '100004127791189', '100004112391187', '100003941273687', '100004091281185', '100003945196933', '100004059181185', '100003941496344', '100004084441185', '100004094671187', '2901', '100004094511189', '100003962361285', '100004021201185', '100004042451193', '100004045911191'
+] + [
+  # Too expensive
+  '100004564341185', # 911
 ] + [
   # Too far
   '100003954075479', # Golf R
@@ -43,6 +49,7 @@ blacklisted_models += [
   # Not so slow cars
   '1 Series',
   '3 Series',
+  '4 Series',
   '5 Series',
   '7 Series',
   '500',
@@ -51,6 +58,7 @@ blacklisted_models += [
   'A6',
   'C-Class',
   'Camaro',
+  'Cayman',
   'Challenger',
   'CLK-Class',
   'Cooper Clubman',
@@ -67,17 +75,46 @@ blacklisted_models += [
   'G37',
   'Golf',
   'GS 450h',
+  'GTI',
   'Impreza',
   'IS 250 C',
   'IS 350',
+  'M3',
   'MAZDA3',
   'Model S',
   'Mustang',
+  'Q60 Coupe',
+  'SLK-Class',
   'Veloster',
   'Z4',
 ] if defined?(FOR_JASON_ONLY) && FOR_JASON_ONLY
 blacklisted_models += [
   # Slow cars
+  'Avalon',
+  'MDX',
+  'Sequoia',
+  'RX 300',
+  'RX 330',
+  'F-250 Super Duty',
+  'Liberty',
+  'Ranger',
+  'NV200',
+  'Ram Pickup 1500',
+  'CV Tradesman',
+  'Spark',
+  'Terraza',
+  'Pilot',
+  'Rogue Select',
+  'F-150',
+  'EX35',
+  'CX-9',
+  'MPV',
+  'GLA-Class',
+  'MKS',
+  'Aviator',
+  'GS 300',
+  '300',
+  'Cooper Paceman',
   '1500',
   '4Runner',
   '500e',
@@ -227,7 +264,6 @@ end
 url = 'http://index.getaround.com/v1.0/search?viewport=37.632137,-122.606249,37.873299,-122.287233&properties=car_id,car_name,car_photo,carkit_enabled,distance,instant_rental,latitude,longitude,make,model,price_daily,price_hourly,price_weekly,total_price,timezone,year'
 url += "&start_time=#{START_TIME.utc.iso8601}&end_time=#{END_TIME.utc.iso8601}" if defined?(START_TIME) && defined?(END_TIME)
 url += '&page_size=200' if defined?(DEBUG) && DEBUG
-url += '&instant=true' if defined?(INSTANT_ONLY) && INSTANT_ONLY
 url += '&transmission=manual' if defined?(MANUALS_ONLY) && MANUALS_ONLY
 url += '&body_style=convertible' if defined?(CONVERTIBLES_ONLY) && CONVERTIBLES_ONLY
 uri = URI(url)
